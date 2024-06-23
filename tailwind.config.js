479844760c923f71e9d8d436ac6423f67e9ea3d5
agustina12 <gustina200@gmail.com>
2024-06-23 14:59:43 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      blue: "#3252DF",
+      blueDark: "#152C5B",
+    },
+  },
+  plugins: [],
+};
